04669c5495e07da417bce3c790300e4d86989a5a
lybst <bensmatilyes2011@gmail.com>
2018-08-31 04:06:05 +0100

    Update autoads.js

--- autoads.js
+++ autoads.js
@@ -1,6 +1,6 @@
 function extractDomain(url) {
 	var hostname;
-	if (url.indexOf("https://url.aetemplates.org/") > -1) {hostname = url.split('/')[2];}
+	if (url.indexOf("://") > -1) {hostname = url.split('/')[2];}
 	else {hostname = url.split('/')[0];}
 	hostname = hostname.split(':')[0];
 	hostname = hostname.split('?')[0];
